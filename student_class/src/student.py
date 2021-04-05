7fb1b01b16ed0f0b8edc506ba12d11114cfb4bbc
hollyhbrady <hollyhbrady@gmail.com>
2021-04-05 16:01:00 +0100

    first commit

---
+++ student_class/src/student.py
@@ -0,0 +1,27 @@
+class Student:
+
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+        # self.fav_lang = fav_lang
+        # self.talk = talk
+
+    current_students = {}
+
+    def student_has_name(self, student_name, student_cohort):
+        self.current_students.append(student_name, student_cohort)
+
+    # def student_can_talk(self, talk):
+    #     talk == "I can talk"
+    #     if self.name:
+    #         return talk
+    def talk(self):
+
+        return "I can talk!"
+
+    def say_favourite_language(self, lang):
+        return "I love " + lang
+
+    
+
+
---
+++ team_class/src/team.py
@@ -0,0 +1,31 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+        # self.points = points
+
+    def team_has_name(self):
+        return name
+
+    def team_has_players(self):
+        return players
+
+    def team_has_coach(self):
+        return coach
+
+    def add_player(self, name):
+        self.players.append(name)
+
+    def has_player(self, name):
+        if name == name:
+            return True
+        
+        return False
+
+    
+        
+
+
+
+
